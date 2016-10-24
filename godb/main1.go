4c2295a6fd68c4c38838a146eaf29a4c611c0f10
chucklapress <charleslapress@gmail.com>
2016-10-23 21:24:46 -0400

    curling tipply/book

--- godb/main1.go
+++ godb/main1.go
@@ -9,7 +9,7 @@ func main() {
     easy := curl.EasyInit()
     defer easy.Cleanup()
 
-    easy.Setopt(curl.OPT_URL, "https://www.smokingpipes.com/")
+    easy.Setopt(curl.OPT_URL, "https://tipply-app.herokuapp.com/book")
 
     // make a callback function
     fooTest := func (buf []byte, userdata interface{}) bool {
